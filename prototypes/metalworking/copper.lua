8d035790095b77718f6c5b139409dedcc8c0c611
HenrikPerden <henrik.perden@gmail.com>
2016-03-22 13:32:11 +0100

    Sifter Copper recipe added
    produces endless copper ore now, change when other ore is added

--- prototypes/metalworking/copper.lua
+++ prototypes/metalworking/copper.lua
@@ -20,6 +20,20 @@ data:extend({
     ingredients = {{"copper-ore",3}},
 		icon = "__base__/graphics/icons/copper-plate.png",
     results = {{"copper-plate",1},{"sand",2},{"ash",1}},
-		order = "a"
+		order = "a-1"
 	}
 })
+--Sifting--
+data:extend({
+{
+	type = "recipe",
+    name = "copper-plate|sifter",
+    category = "sifter",
+	subgroup = "copper",
+    energy_required = 10,
+    ingredients = {{"copper-ore",1}},
+	icon = "__Henrikshell__/graphics/icons/processors/copper-sifting.png",
+    results = {{"copper-ore",2},{"gravel",1}},
+	order = "a-2"
+	}
+	})
