88868c7a0b9af51657eee35da831298592bbd59f
henrikperden <henrik.perden@gmail.com>
2016-03-25 12:00:13 +0100

    Fixed Silver Oxide recipe and added sodium nitrate

--- prototypes/chemicals/silver-processing.lua
+++ prototypes/chemicals/silver-processing.lua
@@ -31,3 +31,34 @@
   },
   }
   )
+  
+    data:extend(
+{
+  {
+    type = "item",
+    name = "silver-oxide",
+    icon = "__Henrikshell__/graphics/icons/materials/silver-oxide.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "silver-base",
+    order = "f[silver-oxide]",
+    stack_size = 100
+  },
+ {
+    type = "recipe",
+    name = "silver-oxide",
+    icon = "__Henrikshell__/graphics/icons/materials/silver-oxide.png",
+    category = "chemistry",
+    subgroup = "silver-base",
+    enabled = true,
+    energy_required = 5,
+    ingredients =
+    {
+     {"silver-nitrate", 2},
+     {"sodium-hydroxide", 2},
+    },
+    results={ {"silver-oxide",1},
+              {"sodium-nitrate",1},
+              {type="fluid", name="water", amount=1}},
+  },
+  }
+  )
---
+++ prototypes/chemicals/sodium-nitrate.lua
@@ -0,0 +1,13 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "sodium-nitrate",
+    icon = "__Henrikshell__/graphics/icons/chemicals/sodium-nitrate.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "silver-processing",
+    order = "f[sodium-nitrate]",
+    stack_size = 100
+  },
+    }
+  )
