c11319b8eb43118f5f3d22f01c7cf0a0005071da
henrikperden <henrik.perden@gmail.com>
2016-03-25 20:18:04 +0100

    Zinc processing added

--- prototypes/metalworking/zinc.lua
+++ prototypes/metalworking/zinc.lua
@@ -1,6 +1,34 @@
 require("prototypes.metalworking.defines")
 data:extend(
 {
+--items--
+ {
+    type = "item",
+    name = "crushed-zinc",
+    icon = "__Henrikshell__/graphics/icons/ore/zinc-ore.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "zinc-processing",
+    order = "c-a-b[zinc-plate]",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "zinc-concentrate",
+    icon = "__Henrikshell__/graphics/icons/ore/zinc-ore.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "zinc-processing",
+    order = "c-a-a[zinc-dust]",
+    stack_size = 200
+  },
+    {
+    type = "item",
+    name = "zinc-oxide",
+    icon = "__Henrikshell__/graphics/icons/ore/zinc-ore.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "zinc-processing",
+    order = "c-a-a[zinc-dust]",
+    stack_size = 200
+  },
  {
     type = "item",
     name = "zinc-plate",
@@ -10,21 +38,56 @@ data:extend(
     order = "c-a-b[zinc-plate]",
     stack_size = 200
   },
+--recipes--
   {
     type = "recipe",
-    name = "zinc-plate",
-    category = "electrolysis",
+    name = "zinc-crushing",
+    icon = "__Henrikshell__/graphics/icons/ore/zinc-ore.png",
+    category = "crusher",
     subgroup = "zinc-processing",
-    energy_required = 3.5,
-    enabled = true,
-    ingredients =
-    {
-      {type="item", name="zinc-ore", amount=1},
-      {type="fluid", name="sulfuric-acid", amount=1}
-    },
-    result = "zinc-plate",
+    order = "a-1",
+    energy_required = 1,
+    enabled = "true",
+    ingredients =  {{type="item", name="zinc-ore", amount=10}},
+    results = {
+                 {type="item", name="crushed-zinc", amount=10},
+              },
   },
-
-
+  {
+    type = "recipe",
+    name = "zinc-sifting",
+    icon = "__Henrikshell__/graphics/icons/ore/zinc-ore.png",
+    category = "sifter",
+    subgroup = "zinc-processing",
+    order="a-2",
+    energy_required = 10,
+    enabled = "true",
+    ingredients = {{"crushed-zinc", 10},{type="fluid", name="water", amount=10}},
+    results = {{"zinc-concentrate", 10},{"gravel",1}},
+  },
+   {
+    type = "recipe",
+    name = "zinc-roasting",
+    icon = "__Henrikshell__/graphics/icons/ore/zinc-ore.png",
+    category = "smelting",
+    subgroup = "zinc-processing",
+    order="a-3",
+    energy_required =  10,
+    enabled = "true",
+    ingredients = {{"zinc-concentrate", 10},{"calcium-oxide",1}},
+    results = {{"zinc-oxide", 10}},
+  },
+ {
+    type = "recipe",
+    name = "zinc-refining",
+    icon = "__Henrikshell__/graphics/icons/metalworking/zinc-plate.png",
+    category = "smelting",
+    subgroup = "zinc-processing",
+    order="a-6",
+    energy_required = 10,
+    enabled = "true",
+    ingredients = {{"zinc-oxide", 10},{"lead-plate",1}},
+    results = {{"zinc-plate", 10},{"lead-plate",1}},
+  },
 }
 )
