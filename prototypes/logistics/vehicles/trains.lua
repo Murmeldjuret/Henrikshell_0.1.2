51ae1000eb25e15f34daf8bd0e225b568f7913aa
henrikperden <henrik.perden@gmail.com>
2016-04-07 13:09:49 +0200

    added an electric train that doesn't work
    code is left but commented out

--- prototypes/logistics/vehicles/trains.lua
+++ prototypes/logistics/vehicles/trains.lua
@@ -17,8 +17,7 @@ data:extend(
     ingredients =
     {
       {"engine-unit", 20},
-      {"electronic-circuit", 10},
-      {"advanced-machine-parts", 30}
+      {"iron-gear-wheel", 30}
     },
     result = "diesel-locomotive"
   },
@@ -205,6 +204,10 @@ data:extend(
     close_sound = { filename = "__base__/sound/car-door-close.ogg", volume = 0.7 },
     sound_minimum_speed = 0.5;
   },
+  }
+  )
+data:extend(
+{
   {
     type = "item",
     name = "cargo-wagon",
@@ -316,3 +319,198 @@ data:extend(
   },
   }
   )
+--[[
+data:extend(
+{
+{
+    type = "item",
+    name = "electric-locomotive",
+    icon = "__base__/graphics/icons/diesel-locomotive.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "trains",
+    order = "a[train-system]-f[electric-locomotive]",
+    place_result = "electric-locomotive",
+    stack_size = 5
+  },
+    {
+    type = "recipe",
+    name = "electric-locomotive",
+    enabled = true,
+    ingredients =
+    {
+      {"electric-engine-unit", 20},
+      {"electronic-circuit", 10},
+      {"advanced-machine-parts", 30}
+    },
+    result = "electric-locomotive"
+  },
+  {
+    type = "locomotive",
+    name = "electric-locomotive",
+    icon = "__base__/graphics/icons/diesel-locomotive.png",
+    flags = {"placeable-neutral", "player-creation", "placeable-off-grid", "not-on-map"},
+    minable = {mining_time = 1, result = "electric-locomotive"},
+    mined_sound = {filename = "__core__/sound/deconstruct-medium.ogg"},
+    max_health = 1000,
+    corpse = "medium-remnants",
+    dying_explosion = "medium-explosion",
+    collision_box = {{-0.6, -2.6}, {0.6, 2.6}},
+    selection_box = {{-0.85, -2.6}, {0.9, 2.5}},
+    drawing_box = {{-1, -4}, {1, 3}},
+    weight = 2000,
+    max_speed = 2,
+    max_power = "600kW",
+    braking_force = 10,
+    friction_force = 0.0015,
+    -- this is a percentage of current speed that will be subtracted
+    air_resistance = 0.002,
+    connection_distance = 3.3,
+    joint_distance = 4.6,
+    energy_per_hit_point = 5,
+    resistances =
+    {
+      {
+        type = "fire",
+        decrease = 15,
+        percent = 50
+      },
+      {
+        type = "physical",
+        decrease = 15,
+        percent = 30
+      },
+      {
+        type = "impact",
+        decrease = 50,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 15,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 10,
+        percent = 20
+      }
+    },
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-input",
+    },
+    front_light =
+    {
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "medium",
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {-0.6, -16},
+        size = 2,
+        intensity = 0.6
+      },
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "medium",
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {0.6, -16},
+        size = 2,
+        intensity = 0.6
+      }
+    },
+    back_light = rolling_stock_back_light(),
+    stand_by_light = rolling_stock_stand_by_light(),
+    pictures =
+    {
+      priority = "very-low",
+      width = 346,
+      height = 248,
+      direction_count = 256,
+      filenames =
+      {
+        "__base__/graphics/entity/diesel-locomotive/diesel-locomotive-01.png",
+        "__base__/graphics/entity/diesel-locomotive/diesel-locomotive-02.png",
+        "__base__/graphics/entity/diesel-locomotive/diesel-locomotive-03.png",
+        "__base__/graphics/entity/diesel-locomotive/diesel-locomotive-04.png",
+        "__base__/graphics/entity/diesel-locomotive/diesel-locomotive-05.png",
+        "__base__/graphics/entity/diesel-locomotive/diesel-locomotive-06.png",
+        "__base__/graphics/entity/diesel-locomotive/diesel-locomotive-07.png",
+        "__base__/graphics/entity/diesel-locomotive/diesel-locomotive-08.png"
+      },
+      line_length = 4,
+      lines_per_file = 8,
+      shift = {1.0, -0.45}
+    },
+    rail_category = "regular",
+
+    stop_trigger =
+    {
+      -- left side
+      {
+        type = "create-smoke",
+        repeat_count = 125,
+        entity_name = "smoke-train-stop",
+        initial_height = 0,
+        -- smoke goes to the left
+        speed = {-0.03, 0},
+        speed_multiplier = 0.75,
+        speed_multiplier_deviation = 1.1,
+        offset_deviation = {{-0.75, -2.7}, {-0.3, 2.7}}
+      },
+      -- right side
+      {
+        type = "create-smoke",
+        repeat_count = 125,
+        entity_name = "smoke-train-stop",
+        initial_height = 0,
+        -- smoke goes to the right
+        speed = {0.03, 0},
+        speed_multiplier = 0.75,
+        speed_multiplier_deviation = 1.1,
+        offset_deviation = {{0.3, -2.7}, {0.75, 2.7}}
+      },
+      {
+        type = "play-sound",
+        sound =
+        {
+          {
+            filename = "__base__/sound/train-breaks.ogg",
+            volume = 0.6
+          },
+        }
+      },
+    },
+    drive_over_tie_trigger = drive_over_tie(),
+    tie_distance = 50,
+    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+    working_sound =
+    {
+      sound =
+      {
+        filename = "__base__/sound/train-engine.ogg",
+        volume = 0.4
+      },
+      match_speed_to_activity = true,
+    },
+    open_sound = { filename = "__base__/sound/car-door-open.ogg", volume=0.7 },
+    close_sound = { filename = "__base__/sound/car-door-close.ogg", volume = 0.7 },
+    sound_minimum_speed = 0.5;
+  },
+  }
+  )
+]]--
